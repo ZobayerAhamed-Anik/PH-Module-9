19290d2e7eabf7cb8c5d794247b1cf2b6c30fce1
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-29 07:19:03 +0600

    Besic while loop

---
+++ Module-9.2/while.js
@@ -0,0 +1,12 @@
+//While loop: syntax -
+//  initialization;
+//      while(condition){
+//          //Repeatative loop body;
+//          increment/decrement;
+//      }
+
+let count = 1;              //initialization
+while(count < 5){           //condition
+    console.log(count);
+    count++;                //increment
+}
